ab3176871008527b492f7184b23acf38fc38660c
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-02-26 23:05:14 +0530

    production build

--- class-two/dist/index.6bd02f5a.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement("hi", {
-    id: "title"
-}, "Heading 1");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("root", root);
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
