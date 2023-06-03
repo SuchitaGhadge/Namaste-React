b9cc37741c086ece845fe435276c690f67d99262
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-06-04 00:04:13 +0530

    component composition

--- class-three/App.js
+++ class-three/App.js
@@ -6,12 +6,25 @@ const heading = React.createElement("h1", {class: "heading"}, "Namaste React �
 
 console.log("heading", heading)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
 // JSX - HTML like or XML like syntax
 // JSX => Babel transpiles it to React.CreateElement => React Element => JS Object => HTML Element(Render)
 const jsxheading = <h1 className="heading">Namaste React 🚀 using JSX</h1> 
 console.log("jsxheading", jsxheading)
-root.render(jsxheading)
+
+const Title = () => (<h1 className="heading">Namaste React 🚀 using Component</h1>)
+
+const HeadingComponent = () => {
+    return (
+        <div>
+        {jsxheading}
+        <Title />
+        <h1>Namaste React Functional Component</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>)
 
 // npx parcel index.html --https   == for build local on https instead of http
