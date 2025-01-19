8260f40e2be1d4e6e36034b6e9305119dc05151d
SuchitaGhadge <suchita8494.sg@gmail.com>
2025-01-19 21:22:21 +0530

    added router using ReactRouter v6

--- class-four/src/App.js
+++ class-four/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import Help from "./components/Help";
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Offer from "./components/Offer";
+import Error from "./components/Error";
 const App = () => {
   return (
     <div className="app">
@@ -13,8 +15,26 @@ const App = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />
+  },
+  {
+    path: '/offers',
+    element: <Offer />
+  },
+  {
+    path: '/help',
+    element: <Help />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <RouterProvider router={appRouter} />
+);
 
 // npx parcel index.html --https   == for build local on https instead of http
---
+++ class-four/src/components/Offer.js
@@ -0,0 +1,10 @@
+const Offer = () => {
+    return (
+        <div>
+            <h2>Offers</h2>
+        </div>
+    )
+}
+
+export default Offer;
+ 
